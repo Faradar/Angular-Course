fd99da2ad0fe2df4c6397bdf38bc6102289736d6
Faradar <tomi_compuamste@hotmail.com>
2025-05-11 14:45:09 -0300

    extract mock db to service and implement async student loading

--- src/app/modules/dashboard/modules/students/students.component.ts
+++ src/app/modules/dashboard/modules/students/students.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ErrorStateMatcher } from '@angular/material/core';
 import { Student } from '../../../../models';
 import { SubmitErrorStateMatcher } from '../../../../errors';
+import { StudentsService } from './students.service';
 
 @Component({
   selector: 'app-students',
@@ -11,9 +12,9 @@ import { SubmitErrorStateMatcher } from '../../../../errors';
   styleUrl: './students.component.scss',
   providers: [
     {
-      // only paint error after submit has been attempted
       provide: ErrorStateMatcher,
       useFactory: (cmp: StudentsComponent) =>
+        // only paint error after submit has been attempted
         new SubmitErrorStateMatcher(() => cmp.submitted),
       deps: [StudentsComponent],
     },
@@ -23,75 +24,34 @@ export class StudentsComponent {
   isEditingId: number | null = null;
   studentForm: FormGroup;
   submitted = false;
+  students: Student[] = [];
+  isLoading = false;
 
-  students: Student[] = [
-    {
-      id: 1,
-      firstName: 'Mary',
-      lastName: 'Smith',
-      email: 'mary.smith@example.com',
-    },
-    {
-      id: 2,
-      firstName: 'John',
-      lastName: 'Doe',
-      email: 'john.doe@example.com',
-    },
-    {
-      id: 3,
-      firstName: 'Alice',
-      lastName: 'Jones',
-      email: 'alice.jones@example.com',
-    },
-    {
-      id: 4,
-      firstName: 'Robert',
-      lastName: 'Brown',
-      email: 'robert.brown@example.com',
-    },
-    {
-      id: 5,
-      firstName: 'Emma',
-      lastName: 'Wilson',
-      email: 'emma.wilson@example.com',
-    },
-    {
-      id: 6,
-      firstName: 'Michael',
-      lastName: 'Lee',
-      email: 'michael.lee@example.com',
-    },
-    {
-      id: 7,
-      firstName: 'Laura',
-      lastName: 'Kim',
-      email: 'laura.kim@example.com',
-    },
-    {
-      id: 8,
-      firstName: 'David',
-      lastName: 'Garcia',
-      email: 'david.garcia@example.com',
-    },
-    {
-      id: 9,
-      firstName: 'Sophia',
-      lastName: 'Martinez',
-      email: 'sophia.martinez@example.com',
-    },
-    {
-      id: 10,
-      firstName: 'Daniel',
-      lastName: 'Wong',
-      email: 'daniel.wong@example.com',
-    },
-  ];
+  constructor(
+    private fb: FormBuilder,
+    private studentService: StudentsService
+  ) {
+    this.loadStudentsObservable();
 
-  constructor(private fb: FormBuilder) {
     this.studentForm = this.fb.group({
       firstName: ['', [Validators.required, Validators.minLength(2)]],
       lastName: ['', [Validators.required, Validators.minLength(2)]],
       email: ['', [Validators.required, Validators.email]],
+    });
+  }
+
+  loadStudentsObservable() {
+    this.isLoading = true;
+    this.studentService.getStudents().subscribe({
+      next: (data) => {
+        this.students = data;
+      },
+      error: (error) => {
+        console.error('There was an error loading the students:', error);
+      },
+      complete: () => {
+        this.isLoading = false;
+      },
     });
   }
 
---
+++ src/app/modules/dashboard/modules/students/students.service.ts
@@ -0,0 +1,82 @@
+import { Injectable } from '@angular/core';
+import { Student } from '../../../../models';
+import { Observable } from 'rxjs';
+
+const MY_FAKE_DB: Student[] = [
+  {
+    id: 1,
+    firstName: 'Mary',
+    lastName: 'Smith',
+    email: 'mary.smith@example.com',
+  },
+  {
+    id: 2,
+    firstName: 'John',
+    lastName: 'Doe',
+    email: 'john.doe@example.com',
+  },
+  {
+    id: 3,
+    firstName: 'Alice',
+    lastName: 'Jones',
+    email: 'alice.jones@example.com',
+  },
+  {
+    id: 4,
+    firstName: 'Robert',
+    lastName: 'Brown',
+    email: 'robert.brown@example.com',
+  },
+  {
+    id: 5,
+    firstName: 'Emma',
+    lastName: 'Wilson',
+    email: 'emma.wilson@example.com',
+  },
+  {
+    id: 6,
+    firstName: 'Michael',
+    lastName: 'Lee',
+    email: 'michael.lee@example.com',
+  },
+  {
+    id: 7,
+    firstName: 'Laura',
+    lastName: 'Kim',
+    email: 'laura.kim@example.com',
+  },
+  {
+    id: 8,
+    firstName: 'David',
+    lastName: 'Garcia',
+    email: 'david.garcia@example.com',
+  },
+  {
+    id: 9,
+    firstName: 'Sophia',
+    lastName: 'Martinez',
+    email: 'sophia.martinez@example.com',
+  },
+  {
+    id: 10,
+    firstName: 'Daniel',
+    lastName: 'Wong',
+    email: 'daniel.wong@example.com',
+  },
+];
+
+@Injectable({
+  providedIn: 'root',
+})
+export class StudentsService {
+  getStudents(): Observable<Student[]> {
+    const studentObservable = new Observable<Student[]>((observer) => {
+      setTimeout(() => {
+        observer.next(MY_FAKE_DB);
+        observer.complete();
+      }, 1000);
+    });
+
+    return studentObservable;
+  }
+}
