278bcc492cabdf4fd49a35634dae82340505a3b8
Faradar <tomi_compuamste@hotmail.com>
2025-05-21 03:16:04 -0300

    edit register component with reactive form and validation

--- src/app/modules/auth/modules/register/register.component.ts
+++ src/app/modules/auth/modules/register/register.component.ts
@@ -1,11 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: false,
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  registerForm!: FormGroup;
+  submitted = false;
 
+  constructor(private fb: FormBuilder, private router: Router) {}
+
+  ngOnInit() {
+    // build the form and add validators
+    this.registerForm = this.fb.group({
+      firstName: ['', [Validators.required, Validators.minLength(2)]],
+      lastName: ['', [Validators.required, Validators.minLength(2)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  // convenience getter for easy access in template
+  get f() {
+    return this.registerForm.controls;
+  }
+
+  get firstName() {
+    return this.registerForm.get('firstName');
+  }
+  get lastName() {
+    return this.registerForm.get('lastName');
+  }
+  get email() {
+    return this.registerForm.get('email');
+  }
+  get password() {
+    return this.registerForm.get('password');
+  }
+
+  register() {
+    this.submitted = true;
+
+    // stop if the form is invalid
+    if (this.registerForm.invalid) {
+      this.registerForm.markAllAsTouched();
+      return;
+    }
+
+    // TODO: call your auth service here
+
+    this.router.navigate(['/dashboard']);
+  }
+
+  backToLogin() {
+    this.router.navigate(['/auth/login']);
+  }
 }
