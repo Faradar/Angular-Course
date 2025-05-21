e198cae2936c286ea39727e365ca5c331f7c1506
Faradar <tomi_compuamste@hotmail.com>
2025-05-20 22:23:20 -0300

    enhanced student detail

--- src/app/modules/dashboard/modules/students/pages/student-detail/student-detail.component.ts
+++ src/app/modules/dashboard/modules/students/pages/student-detail/student-detail.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
-import { Student } from '../../../../../../models';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { forkJoin, Observable } from 'rxjs';
+import { Course, Student } from '../../../../../../models';
 import { StudentsService } from '../../students.service';
+import { CoursesService } from '../../../courses/courses.service';
+import { EnrollmentsService } from '../../../enrollments/enrollments.service';
 
 @Component({
   selector: 'app-student-detail',
@@ -10,18 +12,36 @@ import { StudentsService } from '../../students.service';
   templateUrl: './student-detail.component.html',
   styleUrl: './student-detail.component.scss',
 })
-export class StudentDetailComponent {
-  student: Observable<Student | null>;
+export class StudentDetailComponent implements OnInit {
+  student$!: Observable<Student | null>;
+  enrolledCourses: Course[] = [];
+  loading = true;
 
   constructor(
     private route: ActivatedRoute,
-    private studentService: StudentsService
-  ) {
-    // console.log(this.route);
-    const studentId = this.route.snapshot.params['id'];
+    private router: Router,
+    private studentsSvc: StudentsService,
+    private coursesSvc: CoursesService,
+    private enrollmentsSvc: EnrollmentsService
+  ) {}
 
-    this.student = this.studentService.getStudentById(studentId);
-    console.log('Student Id:', studentId);
-    console.log('Query Params:', this.route.snapshot.queryParams);
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.student$ = this.studentsSvc.getStudentById(id);
+
+    forkJoin({
+      courses: this.coursesSvc.getCourses(),
+      enrollments: this.enrollmentsSvc.getEnrollments(),
+    }).subscribe(({ courses, enrollments }) => {
+      const mine = enrollments.filter((e) => e.studentId === id);
+      this.enrolledCourses = courses.filter((c) =>
+        mine.some((e) => e.courseId === c.id)
+      );
+      this.loading = false;
+    });
+  }
+
+  goBack(): void {
+    this.router.navigate(['/dashboard/students']);
   }
 }
